70fd107dc08e9d20ac4ffb3e7eb6cf41f6493348
championway <cpwearth.eed03@g2.nctu.edu.tw>
2018-03-04 21:55:16 +0800

    finist lab 2

--- catkin_ws/src/marine_duckiebot/marine_duckiebot_navigation/script/odometry_fake.py
+++ catkin_ws/src/marine_duckiebot/marine_duckiebot_navigation/script/odometry_fake.py
@@ -13,61 +13,38 @@ from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseWithCovarianceStamped
 import threading
 class data_transfer():
-	def __init__(self):	
+    def __init__(self): 
 
-		self.world = Odometry()
-		self.world.pose.pose.orientation.w = 1
-		self.odomFilter = Odometry()
-		# Thread
-		self.t1 = threading.Thread(target=self.f1,name='t1')
-		self.t2 = threading.Thread(target=self.f2,name='t2')
-		self.t3 = threading.Thread(target=self.f3,name='t3')
-		self.count = 0
-		self.threadC = threading.Condition()	
+        self.world = Odometry()
+        self.world.pose.pose.orientation.w = 1
+        self.odomFilter = Odometry()
+        # Thread
+        self.count = 0
+        self.threadC = threading.Condition()    
 
-		self.pub = rospy.Publisher('odom', Odometry, queue_size=50)
+        #rospy.Subscriber('/odometry/filtered',Odometry,self.cbOdom)
+        rospy.Subscriber('/p3d_odom',Odometry,self.cbfake)
 
-    		#rospy.Subscriber('/odometry/filtered',Odometry,self.cbOdom)
-    		rospy.Subscriber('/p3d_odom',Odometry,self.cbfake)
+        self.br = tf.TransformBroadcaster()
+        #self.t1.start()
+        #self.t2.start()
+        self.global_tf = 'world'
+        self.model_tf = 'base_footprint'
 
-		self.br = tf.TransformBroadcaster()
-		#self.t1.start()
-		#self.t2.start()
+    def cbfake(self,msg):
+        self.threadC.acquire()
+        self.world.pose.pose.position.x = msg.pose.pose.position.x
+        self.world.pose.pose.position.y = msg.pose.pose.position.y
+        self.world.pose.pose.position.z = msg.pose.pose.position.z
+        self.world.pose.pose.orientation.x = msg.pose.pose.orientation.x
+        self.world.pose.pose.orientation.y = msg.pose.pose.orientation.y
+        self.world.pose.pose.orientation.z = msg.pose.pose.orientation.z
+        self.world.pose.pose.orientation.w = msg.pose.pose.orientation.w    
+        self.br.sendTransform((self.world.pose.pose.position.x,self.world.pose.pose.position.y,self.world.pose.pose.position.z)\
+        ,(self.world.pose.pose.orientation.x,self.world.pose.pose.orientation.y,self.world.pose.pose.orientation.z,self.world.pose.pose.orientation.w),rospy.Time.now(),self.model_tf,self.global_tf)
+        self.threadC.release()
 
- 	def cbfake(self,msg):
-		self.threadC.acquire()
-		self.world.pose.pose.position.x = msg.pose.pose.position.x
-		self.world.pose.pose.position.y = msg.pose.pose.position.y
-		self.world.pose.pose.position.z = msg.pose.pose.position.z
-		self.world.pose.pose.orientation.x = msg.pose.pose.orientation.x
-		self.world.pose.pose.orientation.y = msg.pose.pose.orientation.y
-		self.world.pose.pose.orientation.z = msg.pose.pose.orientation.z
-		self.world.pose.pose.orientation.w = msg.pose.pose.orientation.w	
-		tmp1 = Odometry()	
-		tmp1.child_frame_id = "base_link"
-		tmp1.pose = msg.pose
-		tmp1.twist = msg.twist 
-		tmp1.header.frame_id = "odom"
-		self.br.sendTransform((self.world.pose.pose.position.x,self.world.pose.pose.position.y,self.world.pose.pose.position.z)\
-		,(self.world.pose.pose.orientation.x,self.world.pose.pose.orientation.y,self.world.pose.pose.orientation.z,self.world.pose.pose.orientation.w),rospy.Time.now(),"base_footprint","odom")
-
-		self.pub.publish(tmp1)
-		self.threadC.release()								
-	def f1(self):
-		self.threadC.acquire()
-		self.threadC.wait()
-		self.threadC.release()
-	def f2(self):
-		self.threadC.acquire()
-		self.threadC.wait()
-		self.threadC.release()
-	def f3(self):
-		self.threadC.acquire()
-		self.control()
-		self.threadC.release()
-		self.t3 = threading.Thread(target=self.f3,name='t3')
 if __name__ == '__main__':
      rospy.init_node('odom_fake')
      foo = data_transfer()
      rospy.spin()
-
